87e2653f9f9a0499fb6aa3cb49038e1feb66c07c
bolobob <aokishinichi@gmail.com>
2014-07-06 23:56:55 +0900

    とりあえず箱を用意

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 $(function() {
   // data
   var dataset = [78, 22];
+  var W = 300;
+  var H = 300;
+  var pie = d3.layout.pie();
 
   // append svg
   var svg = d3.select('body')
   .append('svg')
   .attr({
-    'width': 300,
-    'height': 300
+    'width': W,
+    'height': H
+  });
+
+  // create pie chart
+  var arcs = svg.selectAll('.arc')
+  .data(pie(dataset))
+  .enter()
+  .append('g')
+  .classed('arc', true)
+  .attr({
+    'transform': 'translate(' + W/2 + ', '+ H/2 + ')'
   });
 });
